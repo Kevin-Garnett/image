bb2a513fb09a5b9507c4c930fc894f32299f25c7
KG <kevingarnett1984@163.com>
2018-10-04 20:11:01 +0800

    Change to use Asyn to add comment

--- src/main/java/com/hsbc/image/CommentController.java
+++ src/main/java/com/hsbc/image/CommentController.java
@@ -1,19 +1,23 @@
 package com.hsbc.image;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.cloud.stream.reactive.FluxSender;
 import org.springframework.cloud.stream.reactive.StreamEmitter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 @EnableBinding(Source.class)
 public class CommentController {
 
@@ -51,15 +55,19 @@ public class CommentController {
 
     private FluxSink<Message<Comment>> commentSink;
     private Flux<Message<Comment>> flux;
+    private final MeterRegistry meterRegistry;
 
-    public CommentController(){//CounterService counterService){
+    public CommentController(MeterRegistry meterRegistry){//CounterService counterService){
         //this.counterService = counterService;
+        this.meterRegistry = meterRegistry;
         this.flux = Flux.<Message<Comment>>create(
                 emitter -> this.commentSink = emitter,
                 FluxSink.OverflowStrategy.IGNORE
         ).publish().autoConnect();
     }
 
+    // Synchronized one:
+    /*
     @PostMapping("/comments")
     public Mono<String> addComment(Mono<Comment> newComment){
         if (commentSink != null){
@@ -68,6 +76,26 @@ public class CommentController {
             ).then(Mono.just("redirect:/"));
         } else {
             return Mono.just("redirect:/");
+        }
+    }
+    */
+
+    // Asynchronized one:
+    @PostMapping("/comments")
+    public Mono<ResponseEntity<?>> addComment(Mono<Comment> newComment){
+        if (commentSink != null){
+            return newComment.map(comment -> {
+                commentSink.next(MessageBuilder.withPayload(comment)
+                        .setHeader(MessageHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .build());
+                return comment;
+            }).flatMap(comment -> {
+                meterRegistry.counter("comment.produced", "imageId", comment.getImageId())
+                        .increment();
+                return Mono.just(ResponseEntity.noContent().build());
+            });
+        } else {
+            return Mono.just(ResponseEntity.noContent().build());
         }
     }
 
